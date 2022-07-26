852b769285f46c43093a476aca7ffe7c703a052b
Martin Prus <martinprus@hotmail.com>
2022-07-26 11:34:04 -0300

    commit prueba boton

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,12 @@
+import Button from 'react-bootstrap/Button'
+
 function NavBar() {
   return (    
       <nav className="navbar navbar-light ">
         <a className = "nav-item nav-link mx-2" href="google.com">Productos</a>
         <a className = "nav-item nav-link mx-2" href="google.com">Nosotros</a>
-        <a className = "nav-item nav-link mx-2" href="google.com">Comprar</a>
+        <a className = "nav-item nav-link mx-2" href="google.com">Carrito</a>
+        <Button variant = "danger">LogOut</Button>
       </nav>    
   );
 }
