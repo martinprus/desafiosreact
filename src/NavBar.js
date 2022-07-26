3a25cac21bedfda31fb180cc744806a5c66b4765
Martin Prus <martinprus@hotmail.com>
2022-07-26 10:55:30 -0300

    commit desafio menu

---
+++ src/NavBar.js
@@ -0,0 +1,11 @@
+function NavBar() {
+  return (    
+      <nav className="navbar navbar-light ">
+        <a className = "nav-item nav-link mx-2" href="google.com">Productos</a>
+        <a className = "nav-item nav-link mx-2" href="google.com">Nosotros</a>
+        <a className = "nav-item nav-link mx-2" href="google.com">Comprar</a>
+      </nav>    
+  );
+}
+
+export default NavBar;
